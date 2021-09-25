20a31ca111547b2ed6101182671b3ab5ad59af6d
Parijat Singh <mailparijat@gmail.com>
2021-09-25 05:42:11 -0700

    version 6 - third line change

--- src/main/java/com/simplilearn/workshop/controller/PersonController.java
+++ src/main/java/com/simplilearn/workshop/controller/PersonController.java
@@ -7,7 +7,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class PersonController {
 	@RequestMapping("/getname")
 	public String getName() {
-		return " <h1> AWS Code Deploy </h1> <br/> Hello Parijat Singh <br/> <h2> second line change </h2>";
+		String secondLine = "<h2> second line change </h2>";
+		String thirdLine = "<h3> Third line change</h3>";
+		return " <h1> AWS Code Deploy </h1> <br/> Hello Parijat Singh <br/> "
+				+ secondLine
+		+ thirdLine;
 	}
 
 }
